1a51609d058cf4e2d17d0f747dbda89a88aed346
Alex Waychoff <AlexWaychoff@gmail.com>
2017-11-16 09:43:22 -0600

    Started choice 2 switch case, some options started

--- AdventureStory/Pathway.cs
+++ AdventureStory/Pathway.cs
@@ -10,35 +10,68 @@ namespace AdventureStory
     {
         EncounterDescriptions encounterDescriptions = new EncounterDescriptions();
         public int defaultChoice = 0;
+        public int choiceCompleted = 0;
+        public string storyline;
         public string choice1;
         public string choice2;
         public string choice3;
 
         public int GrandAdventure()
         {
-            Console.WriteLine("You're off on a grand adventure!  Barely two steps in you're already left with a choice to make!  The path splits into 3 directions, which way do you go?  ('left', 'middle', or 'right')");
-            choice1 = Console.ReadLine();
-
-            switch (choice1)
+            if (choiceCompleted == 0)
             {
-                case "left":
-                    encounterDescriptions.GBChangeColor();
-                    Console.WriteLine("You venture down the left path.  The sky is " + encounterDescriptions.colorGreenBlue + " and full of " + encounterDescriptions.size + " clouds.  Nothing happens for a while until you come upon a " + encounterDescriptions.firstObject + ".  What do you do?");
-                    break;
-                case "middle":
-                    Console.WriteLine("You venture down the middle path.  You notice some " + encounterDescriptions.colorYellowRed + " wisps floating about the air and the source appears to be coming from a " + encounterDescriptions.thirdObject + ".  What do you do?");
-                    break;
-                case "right":
+                Console.WriteLine("You're off on a grand adventure!  Barely two steps in you're already left with a choice to make!  The path splits into 3 directions, which way do you go?  ('left', 'middle', or 'right')");
+                choice1 = Console.ReadLine();
 
-                    break;
+                switch (choice1)
+                {
+                    case "left":
+                        encounterDescriptions.GBChangeColor();
+                        choiceCompleted += 1;
+                        storyline = "You venture down the left path.  The sky is " + encounterDescriptions.colorGreenBlue + " and full of " + encounterDescriptions.size + " clouds.  Nothing happens for a while until you come upon a " + encounterDescriptions.firstObject + ".  What do you do?";
+                        break;
+                    case "middle":
+                        choiceCompleted += 1;
+                        storyline = "You venture down the middle path.  You notice some " + encounterDescriptions.colorYellowRed + " wisps floating about the air and the source appears to be coming from a " + encounterDescriptions.thirdObject + ".  What do you do?";
+                        break;
+                    case "right":
+                        choiceCompleted += 1;
 
-                default:
-                    defaultChoice += 1;
-                    Console.WriteLine("You hear rumbling nearby.  You're inability to type accurately has apparently disturbed something.");
-                    GrandAdventure();
-                    return 0;
+                        break;
+
+                    default:
+                        defaultChoice += 1;
+                        Console.WriteLine("You hear rumbling nearby.  You're inability to type accurately has apparently disturbed something.");
+                        GrandAdventure();
+                        return 0;
+                }
             }
-            choice2 = Console.ReadLine();
+            if (choiceCompleted == 1)
+            {
+                Console.WriteLine(storyline);
+                Console.WriteLine("You can 'stare', 'yell', or 'comment'");
+                choice2 = Console.ReadLine();
+
+                switch  (choice2)
+                {
+                    case "stare":
+                        storyline = "";
+                        break;
+                    case "yell":
+                        storyline = "";
+                        break;
+                    case "comment":
+
+                        break;
+
+                    default:
+                        defaultChoice += 1;
+                        Console.WriteLine("You hear rumbling nearby.  You're inability to type accurately has apparently disturbed something.");
+                        GrandAdventure();
+                        return 0;
+                }
+
+            }
 
 
             return 0;
